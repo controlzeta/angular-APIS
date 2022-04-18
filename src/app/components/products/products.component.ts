6614d52b3730a601e6dd0bc56204572d02776c4e
Pako Arroyo <francisco.arroyo@controlzeta.com.mx>
2022-04-18 13:17:38 -0500

    Get, POST, PUT

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -15,6 +15,15 @@ export class ProductsComponent implements OnInit {
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  productDetail: Product = {
+    id: '',
+    title: '',
+    price: 0,
+    images: [],
+    description: '',
+    category: { id:'', name : ''}
+  };
+  showProductDetail = false;
 
   constructor(
     private storeService: StoreService,
@@ -27,12 +36,50 @@ export class ProductsComponent implements OnInit {
     this.productsService.getAllProducts()
     .subscribe(data => {
       this.products = data;
+      console.log(data);
     });
   }
 
   onAddToShoppingCart(product: Product) {
     this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
+  }
+
+  onShowProductDetail(id:string){
+    console.log(id);
+    this.productsService.getProduct(id).subscribe(data => {
+      this.productDetail = data
+    });
+    console.log(this.productDetail);
+    this.showProductDetail = true;
+  }
+
+  toggleProductDetail(){
+    this.showProductDetail = !this.showProductDetail;
+  }
+
+  createNewProduct(){
+    this.productsService.createProduct({
+      title: 'New Angular JS Product',
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      categoryId: 1,
+      images : ['https://controlzetacv.firebaseapp.com/assets/img/angular.png'],
+      price: 1
+    }).subscribe(data => {
+      this.productDetail = data;
+      this.products.push(data);
+    });
+  }
+
+  updateProduct(){
+    const changest = { description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+    const id= this.productDetail.id;
+    this.productsService.updateProduct( id, changest).subscribe(data => {
+      console.log('update: ', data);
+      this.productDetail = data;
+      const index = this.products.findIndex(item => item.id === this.productDetail.id);
+      this.products[index] = data;
+    });
   }
 
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,18 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Product } from './../models/product.model';
+import { CreateProductDTO, Product, UpdateProductDTO } from './../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  private apiURL ="https:/young-sands-07814.herokuapp.com/api/products/";
+
   constructor(
     private http: HttpClient
   ) { }
 
   getAllProducts() {
-    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+    return this.http.get<Product[]>(this.apiURL);
+  }
+
+
+  getProduct(id:string){
+    return this.http.get<Product>(this.apiURL + id);
+  }
+
+  createProduct(data:CreateProductDTO){
+    return this.http.post<Product>(this.apiURL, data);
+  }
+
+  updateProduct(id: string, data:UpdateProductDTO){
+    //return this.http.patch<Product>(this.apiURL, data); // Edición de sólo un atributo
+    return this.http.put<Product>(this.apiURL + id, data); // Se envia todo completamente no sólo lo que se cambio
   }
 }
